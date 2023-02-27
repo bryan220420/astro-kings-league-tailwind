22a366245560e46268096d5be434482d5a2d2804
Bryan Armando <mamanibryan1@gmail.com>
2023-02-27 10:46:11 -0500

    reduce code and move to services carpet

--- src/services/getAllTeams.js
+++ src/services/getAllTeams.js
@@ -9,3 +9,18 @@ export const getAllTeams = async () => {
     return [];
   }
 };
+
+export const backgroundColor = {
+  "aniquiladores-fc": "bg-aniquiladores-fc",
+  "1k": "bg-1k",
+  "el-barrio": "bg-el-barrio",
+  "jijantes-fc": "bg-jijantes-fc",
+  kunisports: "bg-kunisports",
+  "los-troncos-fc": "bg-los-troncos-fc",
+  "pio-fc": "bg-pio-fc",
+  "porcinos-fc": "bg-porcinos-fc",
+  "rayo-barcelona": "bg-rayo-barcelona",
+  "saiyans-fc": "bg-saiyans-fc",
+  "ultimate-mostoles": "bg-ultimate-mostoles",
+  "xbuyer-team": "bg-xbuyer-team",
+};
