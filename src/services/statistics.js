15822e0d7d3bd2231a47cdf373be8ac3348fac19
Bryan Armando <mamanibryan1@gmail.com>
2023-03-12 14:37:30 -0500

    add statistics object

--- src/services/statistics.js
+++ src/services/statistics.js
@@ -18,9 +18,25 @@ export async function getTopOnePlayers() {
     } = await fetch(
       "https://kings-league-api.bryan-armando.workers.dev/top-statistics"
     ).then((res) => res.json());
-    return { mvp: topMvp, topScorers: topScorer, topAssist: topAssist };
+    return { mvp: topMvp, topScorers: topScorer, topAssists: topAssist };
   } catch (error) {
     console.log(error);
     return null;
   }
 }
+
+export const typeStatistics = {
+  mvp: { slug: "mvp", title: "MVPs", keyAmount: "mvps", endpoint: "mvp" },
+  topScorers: {
+    slug: "goles",
+    title: "Goles",
+    keyAmount: "goals",
+    endpoint: "top-scorers",
+  },
+  topAssists: {
+    slug: "asistencias",
+    title: "Asistencias",
+    keyAmount: "assists",
+    endpoint: "top-assists",
+  },
+};
